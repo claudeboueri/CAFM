702df35eeecf1e6309bc114d5f7ad42572d6b04c
Claude Boueri <claudeboueri@hotmail.com>
2017-07-25 11:57:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/owner/cafm/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.owner.cafm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+    String user;
+    String pass;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = username.getText().toString().replaceAll("\\s+$","");
+                pass = password.getText().toString();
+            }
+        });
+    }
+}
